2b7d22d3138c69a0d96ab17f30d9db4186d3df57
Atik058 <yatique26@gmail.com>
2026-06-23 13:35:11 +0600

    vercel deployment successfull

--- index.js
+++ index.js
@@ -20,150 +20,172 @@ const client = new MongoClient(uri, {
     }
 });
 
-const plant = {
-    plantName: "Mango",
-    category: "fruit",
-    careLevel: "easy",
-    healthStatus: 60
-};
-
-async function run() {
-    try {
-        console.log("Before route 1");
-
-        app.get('/testin', (req, res) => {
-            res.send('Plant server test in');
-        });
-
-        console.log("Before Mongo");
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-
-        const plantsCollection = client.db('plantDB').collection('plants');
-        const usersCollection = client.db('plantDB').collection('users');
 
 
-        console.log("Mongo connected");
+let plantsCollection;
+let usersCollection;
+let isConnected = false;
 
-        app.get('/testin2', (req, res) => {
-            res.send('Plant server test in under db');
-        });
+async function connectDB() {
+    if (isConnected) return;
 
-        console.log("Route 2 registered");
+    await client.connect();
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    plantsCollection = client.db('plantDB').collection('plants');
+    usersCollection = client.db('plantDB').collection('users');
 
-
-        // app.get('/plants', async (req, res) => {
-        //     const result = await plantsCollection.find().toArray();
-        //     console.log(result)
-        //     res.send(result);
-        // })
-
-        app.get('/my-plants/:email', async (req, res) => {
-            const userEmail = req.params.email;
-            const result = await plantsCollection.find({ userEmail }).toArray();
-            console.log(result)
-            res.send(result);
-        })
-
-        app.post('/addplant', async (req, res) => {
-            const newPlant = req.body
-            console.log(newPlant)
-            const result = await plantsCollection.insertOne(newPlant)
-            res.send(result)
-        })
-
-        app.post('/adduser', async (req, res) => {
-            const newUser = req.body
-            console.log(newUser)
-            const result = await usersCollection.insertOne(newUser)
-            res.send(result)
-        })
-
-        app.get('/plant/:id', async (req, res) => {
-            const id = req.params.id;
-            const projection = { _id: new ObjectId(id) }
-            const result = await plantsCollection.findOne(projection)
-            console.log(result)
-            res.send(result);
-        })
-
-        app.get('/user/:email', async (req, res) => {
-            const email = req.params.email;
-            const projection = { email: email }
-            const result = await usersCollection.findOne(projection)
-            console.log(result)
-            res.send(result);
-        })
-
-        app.put('/update/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) }
-            const updatedPlant = req.body;
-            const updateDocument = {
-                $set: updatedPlant
-            };
-            const options = { upsert: true };
-            const result = await plantsCollection.updateOne(filter, updateDocument, options);
-            res.send(result)
-        })
-
-        app.delete('/plant/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await plantsCollection.deleteOne(query);
-            res.send(result)
-        })
-
-        app.get('/plants', async (req, res) => {
-            const { sortBy } = req.query;
-
-            let result;
-
-            if (sortBy === 'health') {
-                result = await plantsCollection
-                    .aggregate([
-                        {
-                            $sort: {
-                                // nextWateredDate: 1,
-                                healthStatus: -1
-                            }
-                        }
-                    ])
-                    .toArray();
-            }
-            else if (sortBy === 'date') {
-                result = await plantsCollection
-                    .aggregate([
-                        {
-                            $sort: {
-                                nextWateredDate: 1,
-                                // healthStatus: -1
-                            }
-                        }
-                    ])
-                    .toArray();
-            }
-            else {
-                result = await plantsCollection
-                    .find()
-                    .toArray();
-            }
-
-            res.send(result);
-        });
-
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+    isConnected = true;
 }
-run().catch(err => {
-    console.error("RUN ERROR:", err);
+
+app.use(async (req,res,next)=>{
+    try{
+        await connectDB();
+        next();
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).send("Database connection failed");
+    }
 });
+
+app.get('/testin2', (req, res) => {
+    res.send('Plant server test in under db');
+});
+
+console.log("Route 2 registered");
+
+// // Send a ping to confirm a successful connection
+// await client.db("admin").command({ ping: 1 });
+// console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+// app.get('/plants', async (req, res) => {
+//     const result = await plantsCollection.find().toArray();
+//     console.log(result)
+//     res.send(result);
+// })
+
+app.get('/my-plants/:email', async (req, res) => {
+    const userEmail = req.params.email;
+    const result = await plantsCollection.find({ userEmail }).toArray();
+    console.log(result)
+    res.send(result);
+})
+
+app.post('/addplant', async (req, res) => {
+    const newPlant = req.body
+    console.log(newPlant)
+    const result = await plantsCollection.insertOne(newPlant)
+    res.send(result)
+})
+
+app.post('/adduser', async (req, res) => {
+    const newUser = req.body
+    console.log(newUser)
+    const result = await usersCollection.insertOne(newUser)
+    res.send(result)
+})
+
+app.get('/plant/:id', async (req, res) => {
+    const id = req.params.id;
+    const projection = { _id: new ObjectId(id) }
+    const result = await plantsCollection.findOne(projection)
+    console.log(result)
+    res.send(result);
+})
+
+app.get('/user/:email', async (req, res) => {
+    const email = req.params.email;
+    const projection = { email: email }
+    const result = await usersCollection.findOne(projection)
+    console.log(result)
+    res.send(result);
+})
+
+app.put('/update/:id', async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) }
+    const updatedPlant = req.body;
+    const updateDocument = {
+        $set: updatedPlant
+    };
+    const options = { upsert: true };
+    const result = await plantsCollection.updateOne(filter, updateDocument, options);
+    res.send(result)
+})
+
+app.delete('/plant/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await plantsCollection.deleteOne(query);
+    res.send(result)
+})
+
+app.get('/plants', async (req, res) => {
+    const { sortBy } = req.query;
+
+    let result;
+
+    if (sortBy === 'health') {
+        result = await plantsCollection
+            .aggregate([
+                {
+                    $sort: {
+                        // nextWateredDate: 1,
+                        healthStatus: -1
+                    }
+                }
+            ])
+            .toArray();
+    }
+    else if (sortBy === 'date') {
+        result = await plantsCollection
+            .aggregate([
+                {
+                    $sort: {
+                        nextWateredDate: 1,
+                        // healthStatus: -1
+                    }
+                }
+            ])
+            .toArray();
+    }
+    else {
+        result = await plantsCollection
+            .find()
+            .toArray();
+    }
+
+    res.send(result);
+});
+
+// async function run() {
+//     try {
+//         console.log("Before route 1");
+
+//         app.get('/testin', (req, res) => {
+//             res.send('Plant server test in');
+//         });
+
+//         console.log("Before Mongo");
+//         // Connect the client to the server	(optional starting in v4.7)
+//         await client.connect();
+
+//         const plantsCollection = client.db('plantDB').collection('plants');
+//         const usersCollection = client.db('plantDB').collection('users');
+
+
+//         console.log("Mongo connected");
+
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         // await client.close();
+//     }
+// }
+
+// run().catch(err => {
+//     console.error("RUN ERROR:", err);
+// });
 
 
 
